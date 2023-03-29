95dba01d897287750673e772e73ab4367be87b3f
Maksym <maksymRozhko7@gmail.com>
2023-03-29 14:43:02 +0300

    init

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const smoothScrollToElems = (links) => {
+        const anchorsLink = document.querySelectorAll(links);
+      
+        if (anchorsLink) {
+            const smoothScroll = (anchors) => {
+                for (let anchor of anchors) {
+                    const blockID = anchor.getAttribute('href');
+    
+                    anchor.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        if (document.querySelector(blockID)) {
+                            document.querySelector(blockID).scrollIntoView({
+                                behavior: 'smooth',
+                                block: 'start'
+                            });
+                        } else {
+                            try {
+                                localStorage.setItem('scrollTo', 'getStartedSteps');
+                            } catch (error) {}
+                            window.location.href = '/';
+                        }
+                    });
+                };
+            };
+            
+            smoothScroll(anchorsLink);
+        }
+    };
+    
+    smoothScrollToElems('a.scroll-link');
+
+    new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 40,
+        centeredSlides: true,
+        speed: 1000,
+        mousewheel: true,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+    });
+});
